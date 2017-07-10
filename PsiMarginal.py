3037c9cc7119fc6ae928e1067890ea2b416f4d3c
James Cooke <j.cooke@donders.ru.nl>
2017-07-10 11:01:15 +0200

    Added metadata fetch function

--- PsiMarginal.py
+++ PsiMarginal.py
@@ -31,33 +31,35 @@ Prins, N. (2013). The psi-marginal adaptive method: How to give nuisance paramet
     deserve (no more, no less). Journal of Vision, 13(7):3, 1-17.
 """
 
-import numpy as np
-from sklearn.utils.extmath import cartesian
-from scipy.stats import norm, beta, gamma
-from scipy.special import erfc
 import threading
+
 import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+from scipy.special import erfc
+from scipy.stats import norm, beta, gamma
+from sklearn.utils.extmath import cartesian
 
 
 def pf(parameters, psyfun='cGauss'):
     """Generate conditional probabilities from psychometric function.
-    
+
     Arguments
     ---------
         parameters: ndarray (float64) containing parameters as columns
             mu   : threshold
-            
+
             sigma    : slope
-            
+
             gamma   : guessing rate (optional), default is 0.2
-            
+
             lambda  : lapse rate (optional), default is 0.04
-            
+
             x       : stimulus intensity
-            
+
         psyfun  : type of psychometric function.
                 'cGauss' cumulative Gaussian
-            
+
                 'Gumbel' Gumbel, aka log Weibull
 
     Returns
@@ -132,9 +134,9 @@ def GenerateData(parameters, psyfun='cGauss', ntrials=None):
 
 class Psi:
     """Find the stimulus intensity with minimum expected entropy for each trial, to determine the psychometric function.
-  
-    Psi adaptive staircase procedure for use in psychophysics.   
-    
+
+    Psi adaptive staircase procedure for use in psychophysics.
+
     Arguments
     ---------
         stimRange :
@@ -142,66 +144,66 @@ class Psi:
 
         Pfunction (str) : type of psychometric function to use.
             'cGauss' cumulative Gaussian
-            
+
             'Gumbel' Gumbel, aka log Weibull
-        
+
         nTrials :
             number of trials
-            
+
         threshold :
             (alpha) range of possible threshold values to search
-            
+
         thresholdPrior (tuple) : type of prior probability distribution to use.
             Also: slopePrior, guessPrior, lapsePrior.
-            
+
             ('normal',0,1): normal distribution, mean and standard deviation.
-            
+
             ('uniform',None) : uniform distribution, mean and standard deviation not defined.
-            
+
         slope :
             (sigma) range of possible slope values to search
-        
+
         slopePrior :
             see thresholdPrior
-            
+
         guessRate :
             (gamma) range of possible guessing rate values to search
-        
+
         guessPrior :
             see thresholdPrior
-        
+
         lapseRate :
             (lambda) range of possible lapse rate values to search
-        
+
         lapsePrior :
             see thresholdPrior
-        
+
         marginalize (bool) :
             If True, marginalize out the lapse rate and guessing rate before finding the stimulus
             intensity of lowest expected entropy. This uses the Prins (2013) method to include the guessing and lapse rate
             into the probability disctribution. These rates are then marginalized out, and only the threshold and slope are included
             in selection of the stimulus intensity.
-            
+
             If False, lapse rate and guess rate are included in the selection of stimulus intensity.
-    
+
     How to use
     ----------
         Create a psi object instance with all relevant arguments. Selecting a correct search space for the threshold,
         slope, guessing rate and lapse rate is important for the psi procedure to function well. If an estimate for
         one of the parameters ends up at its (upper or lower) limit, the result is not reliable, and the procedure
         should be repeated with a larger search range for that parameter.
-        
+
         Example:
             >>> s   = range(-5,5) # possible stimulus intensities
             obj = Psi(s)
-        
+
         The stimulus intensity to be used in the current trial can be found in the field xCurrent.
-        
+
         Example:
             >>> stim = obj.xCurrent
-        
+
         After each trial, update the psi staircase with the subject response, by calling the addData method.
-        
+
         Example:
             >>> obj.addData(resp)
     """
@@ -326,6 +328,27 @@ class Psi:
             p = np.ones(nx) / nx
         return p
 
+        def meta_data(self):
+            import time
+            import sys
+            metadata = {}
+            date = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))
+            metadata['date'] = date
+            metadata['Version'] = self.version
+            metadata['Python Version'] = sys.version
+            metadata['Numpy Version'] = np.__version__
+            metadata['Scipy Version '] = scipy.__version__
+            metadata['psyFunction'] = self.psyfun
+            metadata['thresholdGrid'] = self.threshold
+            metadata['thresholdPrior'] = self.thresholdPrior
+            metadata['slopeGrid'] = self.slope
+            metadata['slopePrior'] = self.slopePrior
+            metadata['gammaGrid'] = self.guessRate
+            metadata['gammaPrior'] = self.guessPrior
+            metadata['lapseGrid'] = self.lapseRate
+            metadata['lapsePrior'] = self.lapsePrior
+            return metadata
+
     def __entropy(self, pdf):
         """Calculate shannon entropy of posterior distribution.
         Arguments
@@ -396,12 +419,12 @@ class Psi:
     def addData(self, response):
         """
         Add the most recent response to start calculating the next stimulus intensity
-        
+
         Arguments
         ---------
             response: (int)
                 1: correct/right
-                
+
                 0: incorrect/left
         """
         self.stim.append(self.xCurrent)
